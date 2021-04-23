fd475441fab7924ba59a8e3a865d6becc6f701a4
jweaver3 <43688514+jweaver3@users.noreply.github.com>
2021-04-22 21:59:22 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/animalmatchinggame/MainActivity.kt
+++ app/src/main/java/com/example/animalmatchinggame/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.animalmatchinggame
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.Toast
-import kotlinx.androidsynthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 import com.example.animalmatchinggame.R.drawable.*
 import com.example.animalmatchinggame.R.layout.*
 import java.lang.reflect.Array
@@ -22,8 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         var images: MutableList<Int> = mutableListOf(camel, coala, fox, lion, monkey, wolf)
 
-        val buttons = arrayOf(button1, button2, button3, button4, button5, button6, button7, button8,
-            button9, button10, button11, button12, button13, button14)
+        val buttons = arrayOf(button1, button2, button3, button4, button5, button6, button7, button8,button9, button10, button11, button12, button13, button14)
         val cardBack = code
         var clicked = 0
         var turnOver = false
@@ -31,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         images.shuffle()
         for(i in 0..11){
-            buttons[i].setBackgroundResource(cardBack)
+            val backgroundResource = buttons[i].setBackgroundResource(cardBack)
             buttons[i].text = "cardBack"
             buttons[i].textSize = 0.0F
             buttons[i].setOnClickListener {
@@ -43,8 +36,8 @@ class MainActivity : AppCompatActivity() {
                     }
                     clicked++
                 } else if (buttons[i].text !in "cardBack") {
-                    buttons[i].setBackgroundResource(cardBack)
-                    buttons[i].text = "cardBack"
+                    backgroundResource
+                    buttons[i].text= "cardBack"
                     clicked--
                 }
 
